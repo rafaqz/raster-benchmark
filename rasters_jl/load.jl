90a6a051cc4909a6842543d3c57366d4f24ca680
rafaqz <rafaelschouten@gmail.com>
2024-09-24 22:43:41 +0200

    faster

--- rasters_jl/load.jl
+++ rasters_jl/load.jl
@@ -1,4 +1,5 @@
 using Rasters, ArchGDAL, GeoDataFrames
+using Chairmarks
 
 include("utils.jl")
 
@@ -7,9 +8,9 @@ data_dir = joinpath(dirname(@__DIR__), "data")
 points_df = GeoDataFrames.read(joinpath(data_dir, "vector", "points.gpkg"))
 
 raster_dir = joinpath(data_dir, "LC08_L1TP_190024_20200418_20200822_02_T1")
-raster_files = filter(endswith(".TIF"), readdir(raster_dir; join = true))
-rasters = Rasters.Raster.(raster_files; lazy = true)
+raster_files = filter(endswith(".TIF"), readdir(raster_dir; join=true))
 
 # do something
+benchmark = @be RasterStack($raster_files; lazy=true) seconds=10
 
-write_benchmark_as_csv(benchmark; task = "load")
+write_benchmark_as_csv(benchmark; task="load")
